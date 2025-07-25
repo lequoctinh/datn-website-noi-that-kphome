65166038630081331aa2a84f87a612c2825e5a06
Le Quoc Tinh <tinhle2512fpt@gmail.com>
2025-07-25 22:50:02 +0700

    layout homepage ui header

---
+++ client/src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './css/Footer.css';
+function Footer() {
+    return (
+        <div className="ConTaiNer-Footer">
+
+        </div>
+    );
+}
+export default Footer;
